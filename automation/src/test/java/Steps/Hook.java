2e1baf2ae486d3f4ab3a1c6f7ed52c81aa6d4de3
mufleh-ahmad <Mufleh_Ahmad@hotmail.com>
2019-01-12 19:29:52 +0000

    added configuration for grid capabilities

--- automation/src/test/java/Steps/Hook.java
+++ automation/src/test/java/Steps/Hook.java
@@ -4,6 +4,11 @@ import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by Mufleh on 31/12/2018.
@@ -22,8 +27,22 @@ public class Hook extends BaseUtil {
         System.out.println("Starting tests");
         baseUtil.StepInfo = "FirefoxDriver";
 
-        System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver.exe");
-        baseUtil.driver = new ChromeDriver();
+//        System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver.exe");
+//        baseUtil.driver = new ChromeDriver();
+
+
+        //DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+
+        try {
+            baseUtil.driver = new RemoteWebDriver(new URL("http://localhost:4446/wd/hub"), capabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 
     @After
