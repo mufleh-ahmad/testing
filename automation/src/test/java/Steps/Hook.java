8636d56ff2ec92577b72ee54ab132d44e4078c69
mufleh-ahmad <Mufleh_Ahmad@hotmail.com>
2019-01-12 21:17:15 +0000

    added config file

--- automation/src/test/java/Steps/Hook.java
+++ automation/src/test/java/Steps/Hook.java
@@ -7,8 +7,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Properties;
 
 /**
  * Created by Mufleh on 31/12/2018.
@@ -17,43 +22,59 @@ public class Hook extends BaseUtil {
 
     private BaseUtil baseUtil;
 
+    DesiredCapabilities capabilities;
     public Hook(BaseUtil baseUtil) {
         this.baseUtil = baseUtil;
     }
 
+    public String getProperties(String propertyName) {
+        Properties properties = new Properties();
+        InputStream input = null;
+
+        try {
+            input = new FileInputStream("src/test/resources/config.properties");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        // load a properties file
+        try {
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // get the property value and print it out
+        return properties.getProperty(propertyName);
+    }
+
     @Before
-    public void initialize(){
+    public void initialize() throws MalformedURLException {
+
+        String browserProperty = getProperties("browser");
+        String seleniumHubUrl = getProperties("selenium-hub-url");
 
         System.out.println("Starting tests");
         baseUtil.StepInfo = "FirefoxDriver";
 
-//        System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver.exe");
-//        baseUtil.driver = new ChromeDriver();
-
-
-        //DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-
-        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-
-        try {
-            baseUtil.driver = new RemoteWebDriver(new URL("http://localhost:4446/wd/hub"), capabilities);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
+        if (browserProperty.equals("Chrome")) {
+            System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver.exe");
+            baseUtil.driver = new ChromeDriver();
+        } else if (browserProperty.equals("ChromeGrid")) {
+            capabilities = DesiredCapabilities.chrome();
+            baseUtil.driver = new RemoteWebDriver(new URL(seleniumHubUrl), capabilities);
+        } else if (browserProperty.equals("FirefoxGrid")){
+            capabilities = DesiredCapabilities.firefox();
+            baseUtil.driver = new RemoteWebDriver(new URL(seleniumHubUrl), capabilities);
         }
-
-
-
     }
 
     @After
-
     public void teardown(Scenario scenario){
-
         if (scenario.isFailed()){
             System.out.println("test failed");
         }
 
-        baseUtil.driver.close();
+        baseUtil.driver.quit();
         System.out.println("Test ended");
     }
 
